70b9031e7d2678f2274dd6f984a2c4ad905d2a8a
Joseph McCann <joe.mccann@mailbox.org>
2025-04-15 11:54:50 -0400

    Make a sphere model rotate. Sphere stops rotating when freeze param enabled.

--- Source/ui/public/js/animated.js
+++ Source/ui/public/js/animated.js
@@ -41,7 +41,7 @@ const visualizerStyle = getComputedStyle(visualizer);
 renderer.setSize(parseInt(visualizerStyle.width), parseInt(visualizerStyle.height));
 visualizer.appendChild(renderer.domElement);
 
-camera.position.set(10, -5, 20);
+camera.position.set(-10, -5, 20);
 camera.lookAt(0, 3, 0);
 
 const loader = new GLTFLoader();
@@ -50,25 +50,16 @@ const mixer = new AnimationMixer();
 // https://sketchfab.com/3d-models/blue-flower-animated-c20b1f12833148e09f7f49c3dd444906
 let animationClip;
 let threeD_Object;
-loader.load('assets/Blue_end.glb', function (glb) {
+loader.load('assets/sphere.glb', function (glb) {
 
     threeD_Object = glb.scene;
-    threeD_Object.scale.set(200, 200, 200);
+    threeD_Object.scale.set(1, 1, 1);
+    console.log(threeD_Object);
 
-    animationClip = glb.animations[0];
-    //const track0 = animationClip["tracks"][0];
-    let duration = animationClip["duration"];
-    //duration = 20.0;
-    animationClip.duration = duration;
-    //console.log("duration: ", duration);
-    //let track0_times = track0["times"][0];
-    //track0_times = 0.56;
-    //console.log("track0_times:", track0_times);
-    //console.log("track0: ", track0);
-    ////animaionClip.duration = 4;
-    console.log("animationClip: ", animationClip);
-    let action = mixer.clipAction(animationClip, threeD_Object);
-    action.play();
+    //animationClip = glb.animations[0];
+
+    //let action = mixer.clipAction(animationClip, threeD_Object);
+    //action.play();
     scene.add(threeD_Object);
 
 }, undefined, function (error) {
@@ -76,10 +67,10 @@ loader.load('assets/Blue_end.glb', function (glb) {
 });
 
 const light = new DirectionalLight(0xffffed, 1);
-light.position.set(1, 1, 5);
+light.position.set(-10, 10, 10);
 scene.add(light);
 
-const ambientLight = new AmbientLight(0x404040);
+const ambientLight = new AmbientLight(0xffffed, 1);
 scene.add(ambientLight);
 
 scene.background = new Color(mediumDarkGray);
@@ -96,7 +87,7 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     if (!UI.freezeCheckbox.checked) {
-        threeD_Object.rotation.y += 0.01;
+        threeD_Object.rotation.y += 0.001;
     }
     mixer.update(1 / 60);
 }
