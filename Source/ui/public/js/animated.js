977045e294cacb07279b32f7fdde49f9fe0b692f
Joseph McCann <joe.mccann@mailbox.org>
2025-06-17 13:52:24 -0400

    Add particle system example as potential animation inclusion.

--- Source/ui/public/js/animated.js
+++ Source/ui/public/js/animated.js
@@ -26,6 +26,7 @@ import ParticleSystem, {
     Force,
 } from 'three-nebula';
 import * as UI from './index.js';
+import Stats from 'three/addons/libs/stats.module.js';
 
 // COLORS
 const freezeColor = 0x2c2e54;
@@ -62,6 +63,9 @@ renderer.setSize(parseInt(visualizerStyle.width), parseInt(visualizerStyle.heigh
 renderer.shadowMap.enabled = true;
 const canvas = renderer.domElement;
 visualizer.appendChild(canvas);
+
+let stats = new Stats();
+visualizer.appendChild(stats.dom)
 
 const width = canvas.width;
 const height = canvas.height;
@@ -437,6 +441,77 @@ system
     .addEmitter(emitterRight1)
     .addRenderer(new SpriteRenderer(scene, THREE));
 
+// testing example code: https://github.com/mrdoob/three.js/blob/master/examples/webgl_points_waves.html
+const SEPARATION = 10, AMOUNTX = 20, AMOUNTY = 15;
+const WAVE_X_POS = 50, WAVE_Y_POS = 255, WAVE_Z_POS = 50;
+let particles, count = 0;
+const numParticles = AMOUNTX * AMOUNTY;
+const positions = new Float32Array(numParticles * 3);
+const scales = new Float32Array(numParticles);
+
+let i = 0, j = 0;
+
+for (let ix = 0; ix < AMOUNTX; ix++) {
+
+    for (let iy = 0; iy < AMOUNTY; iy++) {
+
+        positions[i] = WAVE_X_POS + ix * SEPARATION - ((AMOUNTX * SEPARATION) / 2); // x
+        positions[i + 1] = WAVE_Y_POS; // y
+        positions[i + 2] = WAVE_Z_POS + iy * SEPARATION - ((AMOUNTY * SEPARATION) / 2); // z
+
+        scales[j] = 1;
+
+        i += 3;
+        j++;
+
+    }
+
+}
+
+const buffGeometry = new THREE.BufferGeometry();
+buffGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+buffGeometry.setAttribute('scale', new THREE.BufferAttribute(scales, 1));
+
+const shaderMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        color: { value: new THREE.Color(0xfafaf0) },
+        size: { value: 0.12 }
+    },
+    vertexShader: document.getElementById('vertexshader').textContent,
+    fragmentShader: document.getElementById('fragmentshader').textContent,
+})
+
+particles = new THREE.Points(buffGeometry, shaderMaterial);
+scene.add(particles);
+
+
+function animateParticles() {
+    const positions = particles.geometry.attributes.position.array;
+    const scales = particles.geometry.attributes.scale.array;
+
+    let i = 0, j = 0;
+
+    for (let ix = 0; ix < AMOUNTX; ix++) {
+
+        for (let iy = 0; iy < AMOUNTY; iy++) {
+
+            positions[i + 1] = WAVE_Y_POS + (Math.sin((ix + count) * 0.3) * 50) +
+                (Math.sin((iy + count) * 0.5) * 50);
+
+            scales[j] = (Math.sin((ix + count) * 0.3) + 1) * 20 +
+                (Math.sin((iy + count) * 0.5) + 1) * 20;
+
+            i += 3;
+            j++;
+
+        }
+
+    }
+    particles.geometry.attributes.position.needsUpdate = true;
+    particles.geometry.attributes.scale.needsUpdate = true;
+}
+
+// end testing example code
 function animate(time, theta = 0, emitterRadius = 10) {
     time *= 0.001;
 
@@ -476,10 +551,13 @@ function animate(time, theta = 0, emitterRadius = 10) {
     const rotation = time * bigSphereRotationSpeed;
     bigSphere.rotation.y = rotation;
 
+    animateParticles();
+
+    stats.update();
     system.update();
     controls.update();
     renderer.render(scene, camera);
-
+    count += 0.1;
     requestAnimationFrame((time) => animate(time, theta, emitterRadius));
 }
 
